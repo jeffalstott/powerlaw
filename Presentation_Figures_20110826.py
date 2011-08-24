b7ed50884c16b4befe2f54f136f743c086026e26
Jeff Alstott <jeffalstott@gmail.com>
2011-08-24 15:24:47 -0400

    Added Monkey K2 support

--- Presentation_Figures_20110826.py
+++ Presentation_Figures_20110826.py
@@ -9,7 +9,9 @@ figure_directory = '/home/jja34/public_html/Figures/'
 data_directory = '/work/imaging8/jja34/ECoG_Study/ECoG_Data/'
 filter = 'filter_FIR_513_blackmanharris'
 
-monkeys = (('A', 'food_tracking'), ('K1', 'food_tracking'), ('K2', 'visual_grating'), ('K2', 'rest'), ('K2', 'anesthesia'))
+monkeys = (('A', 'food_tracking0'),('A', 'food_tracking1'),('A', 'food_tracking2'),('A', 'food_tracking3'),('A', 'food_tracking4'),\
+        ('K1', 'food_tracking0'), ('K1', 'food_tracking1'),('K1', 'food_tracking2'),
+        ('K2', 'visual_grating'), ('K2', 'rest'), ('K2', 'anesthesia'))
 bands = ('beta', 'alpha', 'theta', 'delta')
 recordings = range(5)
 methods = (('events', 1), ('displacements', 2), ('amplitudes', 3), ('amplitude_aucs', 4))
@@ -18,26 +20,22 @@ for monkey, task in monkeys:
     for band in bands:
         if monkey == 'A':
             b = 3
-            recordings = range(5)
         if monkey=='K1':
-            recordings = range(3)
             b = 1
         if monkey=='K2':
             b = 1
-            recordings = ''
             if task!='visual_grating':
                 filter = 'filter_version0'
-        for i in recordings:
-            f = h5py.File(data_directory+'Monkey_'+monkey+'.hdf5')
-            data = f[task+str(i)+'/'+filter+'/'+band]
-            d = criticality.avalanche_analysis(data, bin_width=b)
+        f = h5py.File(data_directory+'Monkey_'+monkey+'.hdf5')
+        data = f[task+'/'+filter+'/'+band]
+        d = criticality.avalanche_analysis(data, bin_width=b)
         
-            for method, fig in methods:
-                X = d['size_'+method]
-                plt.figure(fig)
-                statistics.hist_log(X, X.max(), X.min())
-                plt.xlabel('Size ('+method+')', fontsize='xx-large')
-                plt.ylabel('P(Size)', fontsize='xx-large')
-                plt.savefig(figure_directory+task+'_'+band+'_'+method+'_'+monkey)
-                plt.title('Sizes as '+method+', '+band+' band,  Monkey '+monkey, fontsize='xx-large')
+        for method, fig in methods:
+            X = d['size_'+method]
+            plt.figure(fig)
+            statistics.hist_log(X, X.max(), X.min())
+            plt.xlabel('Size ('+method+')', fontsize='xx-large')
+            plt.ylabel('P(Size)', fontsize='xx-large')
+            plt.savefig(figure_directory+task+'_'+band+'_'+method+'_'+monkey)
+            plt.title('Sizes as '+method+', '+band+' band,  Monkey '+monkey, fontsize='xx-large')
         plt.close('all')
