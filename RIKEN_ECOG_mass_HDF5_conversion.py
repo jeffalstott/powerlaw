c8cf0cf49d26a5bfae50146bcbbeab753a0306f5
Jeff Alstott <jeffalstott@gmail.com>
2011-08-30 14:02:20 -0400

    Doing monkey processing at Helix

--- RIKEN_ECOG_mass_HDF5_conversion.py
+++ RIKEN_ECOG_mass_HDF5_conversion.py
@@ -1,8 +1,8 @@
 import os
 from input_processing import riken_import, write_to_HDF5
 
-path = '/work/imaging8/jja34/ECoG_Study/ECoG_Data/data'
-#path = '/home/alstottj/data'
+#path = '/work/imaging8/jja34/ECoG_Study/ECoG_Data/data'
+path = '/home/alstottj/data'
 dirList=os.listdir(path)
 
 #bands = ('delta', 'theta', 'alpha', 'beta')
@@ -14,7 +14,8 @@ tasks = {'FTT': 'food_tracking', 'EMT': 'emotional_movie', 'VGT': 'visual_gratin
 A_counter = -1
 K1_counter = -1
 for dirname in dirList:
-    data = riken_import(dirname)
+    filename = path+'/'+dirname
+    data = riken_import(filename)
     print dirname
     components = dirname.split('_')
     name = components[2]
@@ -26,8 +27,8 @@ for dirname in dirList:
         K1_counter = K1_counter+1
         task = task+str(K1_counter)
 
-    output_file = '/work/imaging8/jja34/ECoG_Study/ECoG_Data/Monkey_'+name
-    #output_file = '/scratch/alstottj/Monkey_'+name
+    #output_file = '/work/imaging8/jja34/ECoG_Study/ECoG_Data/Monkey_'+name
+    output_file = '/scratch/alstottj/Monkey_'+name
     write_to_HDF5(data,output_file, task, 1000.0, bands=bands)
 
 
