56627b438b40c15ec6c566151d72c98ec83c2e22
Jeff Alstott <jeffalstott@gmail.com>
2011-08-31 16:32:43 -0400

    Further prep for Group 2 conversion

--- BCNI_MEG_mass_HDF5_conversion.py
+++ BCNI_MEG_mass_HDF5_conversion.py
@@ -1,5 +1,5 @@
 import os
-from input_processing import mrc_fieldtrip_import, write_to_HDF5
+from input_processing import mrc_raw_import, write_to_HDF5
 
 path = '/home/jja34/public_html/MEG_Data/Rest/Group2'
 dirList=os.listdir(path)
@@ -9,7 +9,7 @@ bands = ('delta', 'theta', 'alpha', 'beta', 'gamma', 'high-gamma', 'raw', 'broad
 for fname in dirList:
     if fname[0]=='f':
         filename = path+'/'+fname
-        data = mrc_fieldtrip_import(filename)
+        data = mrc_raw_import(filename)
         subject_id = fname[31:34]
         eyes = fname[26:30]
         visit = fname[37]
