c2971cc2e561ba12308d2f38fd5176057d594f25
Jeff Alstott <jeffalstott@gmail.com>
2011-09-02 14:07:17 -0400

    Added statistical analysis code to criticality.py. Set things up to preprocess two subjects I had to delete.

--- BCNI_MEG_mass_HDF5_conversion.py
+++ BCNI_MEG_mass_HDF5_conversion.py
@@ -1,22 +1,22 @@
 import os
-from input_processing import mrc_raw_import, write_to_HDF5
+from input_processing import mrc_fieldtrip_import, write_to_HDF5
 
-path = '/home/jja34/public_html/MEG_Data/Rest/Group2'
+path = '/home/jja34/public_html/MEG_Data/Rest/Group1'
 dirList=os.listdir(path)
 
 bands = ('delta', 'theta', 'alpha', 'beta', 'gamma', 'high-gamma', 'raw', 'broad')
 
 for fname in dirList:
-    if fname[0]=='r':
+    components = fname.split('_')
+    if fname[6]=='001' or fname[6]=='002':
         filename = path+'/'+fname
-        data = mrc_raw_import(filename)
-        components = fname.split('_')
+        data = mrc_fieldtrip_import(filename)
         subject_id = components[6]
         eyes = components[5]
         visit = components[8]
         print fname
         print subject_id+eyes+visit
-        output_file = '/work/imagingA/jja34/MEG_Study/Data/Subject'+subject_id
+        output_file = '/work/imagingA/jja34/MEG_Study/Data/Group1/Subject'+subject_id
         condition = visit+'/rest/'+eyes+'/magnetometer'
         write_to_HDF5(data['magnetometer'],output_file, condition, 250.0, bands=bands)
         condition = visit+'/rest/'+eyes+'/gradiometer'
