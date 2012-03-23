dd2995987d89219aec3185a3839f0c04badb7c87
Jeff Alstott <jeffalstott@gmail.com>
2012-03-23 16:56:29 -0400

    New version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
         name = 'powerlaw',
         py_modules = ['powerlaw'],
-        version = '.4',
+        version = '.4.1',
         description = 'Toolbox for testing if a probability distribution fits a power law',
         author='Jeff Alstott',
         author_email = 'jeffalstott@gmail.com',
         url = 'http://code.google.com/p/powerlaw/',
-        requires = ['scipy', 'numpy', 'matplotlib'],
+        requires = ['scipy', 'numpy', 'matplotlib', 'mpmath'],
         license = 'MIT',
         classifiers = [
             'License :: OSI Approved :: MIT License',
